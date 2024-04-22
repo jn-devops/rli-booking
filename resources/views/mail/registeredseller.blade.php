5be720632f76809a41a5bb55732a0177a6f8488c
clandrade12 <clandrade@joy-nostalg.com>
2024-04-22 14:30:26 +0800

    update seller's registration notification

--- resources/views/mail/registeredseller.blade.php
+++ resources/views/mail/registeredseller.blade.php
@@ -7,16 +7,16 @@ We are thrilled to have you on board. Your access has been set up, and you can n
 
 To log in to the Seller's Portal and to your email account, please use the following credentials:<br>
 
-Seller's Portal Link: <a href="https://book-dev.enclaves.ph/">https://book-dev.enclaves.ph/</a><br>
-Email Link: <a href="https://enclaves.ph/roundcube_enclaves/">https://enclaves.ph/roundcube_enclaves/</a><br>
+<a href="https://book.homeful.ph/">Seller's Portal</a>
+<a href="https://enclaves.ph/roundcube_enclaves/">Seller's Email</a><br>
 Username: <b>{{$seller->email}}</b><br>
 Password: <b>{{$password}}</b><br>
 
 Kindly make sure to <b>fill-out the Disbursement Enrollment Form for your commission</b> by following the steps below and <b>upload your accreditation Documents at <a href="https://jng.ph/seller-document">Seller Attachment Form</a></b>.<br>
 
 Fill-out Disbursement Form:
-1. Login to <a href="https://book-dev.enclaves.ph/user/profile">Seller's Portal.</a><br>
-2. Once you're logged in, click your name at the upper corner of your screen then click Profile or click <a href="https://book-dev.enclaves.ph/user/profile">here</a>.<br>  
+1. Login to <a href="https://book.homeful.ph/user/profile">Seller's Portal.</a><br>
+2. Once you're logged in, click your name at the upper corner of your screen then click Profile or click <a href="https://book.homeful.ph/user/profile">here</a>.<br>  
 3. Update Account Disbursement section then click SAVE.<br>
 
 Should you have any questions, please do not hesitate to contact us at customer.care@joy-nostalg.com<br>
